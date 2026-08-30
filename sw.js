fa2fddac56d375126ad55b368dd8e35b2bd55d56
vvlietsam-lab <vvlietsam@gmail.com>
2026-08-30 15:16:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* RONDO service worker — maakt het spel offline speelbaar */
-const CACHE = "rondo-v39";
+const CACHE = "rondo-v43";
 const BESTANDEN = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable.png"];
 
